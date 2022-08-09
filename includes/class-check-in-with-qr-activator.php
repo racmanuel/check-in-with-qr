40e83b92a79b9c171f41b9c4edaaa895568973dd
Manuel Ramirez Coronel <ra_cm@outlook.com>
2022-08-09 10:08:20 -0500

    make a wp table on active plugin

--- includes/class-check-in-with-qr-activator.php
+++ includes/class-check-in-with-qr-activator.php
@@ -20,145 +20,178 @@
  * @subpackage Check_In_With_Qr/includes
  * @author     Manuel Ramirez Coronel <ra_cm@outlook.com>
  */
-class Check_In_With_Qr_Activator {
+class Check_In_With_Qr_Activator
+{
 
-	/**
-	 * The $_REQUEST during plugin activation.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      array    $request    The $_REQUEST array during plugin activation.
-	 */
-	private static $request = array();
+    /**
+     * The $_REQUEST during plugin activation.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      array    $request    The $_REQUEST array during plugin activation.
+     */
+    private static $request = array();
 
-	/**
-	 * The $_REQUEST['plugin'] during plugin activation.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin    The $_REQUEST['plugin'] value during plugin activation.
-	 */
-	private static $plugin = CHECK_IN_WITH_QR_BASE_NAME;
+    /**
+     * The $_REQUEST['plugin'] during plugin activation.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin    The $_REQUEST['plugin'] value during plugin activation.
+     */
+    private static $plugin = CHECK_IN_WITH_QR_BASE_NAME;
 
-	/**
-	 * Activate the plugin.
-	 *
-	 * Checks if the plugin was (safely) activated.
-	 * Place to add any custom action during plugin activation.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
+    /**
+     * Activate the plugin.
+     *
+     * Checks if the plugin was (safely) activated.
+     * Place to add any custom action during plugin activation.
+     *
+     * @since    1.0.0
+     */
+    public static function activate()
+    {
 
-		if ( false === self::get_request()
-			|| false === self::validate_request( self::$plugin )
-			|| false === self::check_caps()
-		) {
-			if ( isset( $_REQUEST['plugin'] ) ) {
-				if ( ! check_admin_referer( 'activate-plugin_' . self::$request['plugin'] ) ) {
-					exit;
-				}
-			} elseif ( isset( $_REQUEST['checked'] ) ) {
-				if ( ! check_admin_referer( 'bulk-plugins' ) ) {
-					exit;
-				}
-			}
-		}
+        if (false === self::get_request()
+            || false === self::validate_request(self::$plugin)
+            || false === self::check_caps()
+        ) {
+            if (isset($_REQUEST['plugin'])) {
+                if (!check_admin_referer('activate-plugin_' . self::$request['plugin'])) {
+                    exit;
+                }
+            } elseif (isset($_REQUEST['checked'])) {
+                if (!check_admin_referer('bulk-plugins')) {
+                    exit;
+                }
+            }
+        }
 
-		/**
-		 * The plugin is now safely activated.
-		 * Perform your activation actions here.
-		 */
+        /**
+         * The plugin is now safely activated.
+         * Perform your activation actions here.
+         */
+		self::create_tables();
+    }
 
-	}
+    /**
+     * Get the request.
+     *
+     * Gets the $_REQUEST array and checks if necessary keys are set.
+     * Populates self::request with necessary and sanitized values.
+     *
+     * @since    1.0.0
+     * @return bool|array false or self::$request array.
+     */
+    private static function get_request()
+    {
 
-	/**
-	 * Get the request.
-	 *
-	 * Gets the $_REQUEST array and checks if necessary keys are set.
-	 * Populates self::request with necessary and sanitized values.
-	 *
-	 * @since    1.0.0
-	 * @return bool|array false or self::$request array.
-	 */
-	private static function get_request() {
+        if (!empty($_REQUEST)
+            && isset($_REQUEST['_wpnonce'])
+            && isset($_REQUEST['action'])
+        ) {
+            if (isset($_REQUEST['plugin'])) {
+                if (false !== wp_verify_nonce(sanitize_text_field(wp_unslash($_REQUEST['_wpnonce'])), 'activate-plugin_' . sanitize_text_field(wp_unslash($_REQUEST['plugin'])))) {
 
-		if ( ! empty( $_REQUEST )
-			&& isset( $_REQUEST['_wpnonce'] )
-			&& isset( $_REQUEST['action'] )
-		) {
-			if ( isset( $_REQUEST['plugin'] ) ) {
-				if ( false !== wp_verify_nonce( sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ), 'activate-plugin_' . sanitize_text_field( wp_unslash( $_REQUEST['plugin'] ) ) ) ) {
+                    self::$request['plugin'] = sanitize_text_field(wp_unslash($_REQUEST['plugin']));
+                    self::$request['action'] = sanitize_text_field(wp_unslash($_REQUEST['action']));
 
-					self::$request['plugin'] = sanitize_text_field( wp_unslash( $_REQUEST['plugin'] ) );
-					self::$request['action'] = sanitize_text_field( wp_unslash( $_REQUEST['action'] ) );
+                    return self::$request;
 
-					return self::$request;
+                }
+            } elseif (isset($_REQUEST['checked'])) {
+                if (false !== wp_verify_nonce(sanitize_text_field(wp_unslash($_REQUEST['_wpnonce'])), 'bulk-plugins')) {
 
-				}
-			} elseif ( isset( $_REQUEST['checked'] ) ) {
-				if ( false !== wp_verify_nonce( sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ), 'bulk-plugins' ) ) {
+                    self::$request['action'] = sanitize_text_field(wp_unslash($_REQUEST['action']));
+                    self::$request['plugins'] = array_map('sanitize_text_field', wp_unslash($_REQUEST['checked']));
 
-					self::$request['action'] = sanitize_text_field( wp_unslash( $_REQUEST['action'] ) );
-					self::$request['plugins'] = array_map( 'sanitize_text_field', wp_unslash( $_REQUEST['checked'] ) );
+                    return self::$request;
 
-					return self::$request;
+                }
+            }
+        } else {
 
-				}
-			}
-		} else {
+            return false;
+        }
 
-			return false;
-		}
+    }
 
-	}
+    /**
+     * Validate the Request data.
+     *
+     * Validates the $_REQUESTed data is matching this plugin and action.
+     *
+     * @since    1.0.0
+     * @param string $plugin The Plugin folder/name.php.
+     * @return bool false if either plugin or action does not match, else true.
+     */
+    private static function validate_request($plugin)
+    {
 
-	/**
-	 * Validate the Request data.
-	 *
-	 * Validates the $_REQUESTed data is matching this plugin and action.
-	 *
-	 * @since    1.0.0
-	 * @param string $plugin The Plugin folder/name.php.
-	 * @return bool false if either plugin or action does not match, else true.
-	 */
-	private static function validate_request( $plugin ) {
+        if (isset(self::$request['plugin'])
+            && $plugin === self::$request['plugin']
+            && 'activate' === self::$request['action']
+        ) {
 
-		if ( isset( self::$request['plugin'] )
-			&& $plugin === self::$request['plugin']
-			&& 'activate' === self::$request['action']
-		) {
+            return true;
 
-			return true;
+        } elseif (isset(self::$request['plugins'])
+            && 'activate-selected' === self::$request['action']
+            && in_array($plugin, self::$request['plugins'])
+        ) {
+            return true;
+        }
 
-		} elseif ( isset( self::$request['plugins'] )
-			&& 'activate-selected' === self::$request['action']
-			&& in_array( $plugin, self::$request['plugins'] )
-		) {
-			return true;
-		}
+        return false;
 
-		return false;
+    }
 
-	}
+    /**
+     * Check Capabilities.
+     *
+     * We want no one else but users with activate_plugins or above to be able to active this plugin.
+     *
+     * @since    1.0.0
+     * @return bool false if no caps, else true.
+     */
+    private static function check_caps()
+    {
 
-	/**
-	 * Check Capabilities.
-	 *
-	 * We want no one else but users with activate_plugins or above to be able to active this plugin.
-	 *
-	 * @since    1.0.0
-	 * @return bool false if no caps, else true.
-	 */
-	private static function check_caps() {
+        if (current_user_can('activate_plugins')) {
+            return true;
+        }
 
-		if ( current_user_can( 'activate_plugins' ) ) {
-			return true;
-		}
+        return false;
 
-		return false;
+    }
 
-	}
+    private static function create_tables()
+    {
+        global $wpdb;
+        global $check_in_out_db_version;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $table_name = $wpdb->prefix . 'check_in_out';
+
+        $table_create = "CREATE TABLE $table_name (
+			`id` INT(11) NOT NULL AUTO_INCREMENT,
+			`id_user` INT(11) NOT NULL,
+			`date` DATE NOT NULL,
+			`time` TIME NOT NULL,
+			`year` YEAR NOT NULL,
+			`check_in` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+			PRIMARY KEY (`id`) USING BTREE
+		)
+		$charset_collate
+		ENGINE=InnoDB
+		;";
+
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        
+		dbDelta($table_create);
+
+        add_option('check_in_out_db_version', $check_in_out_db_version);
+    }
 
 }
-
